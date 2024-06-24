4efd192c67aa12bb7c0b9efe466cc0ba7da376ef
Matheus Mendes <matheuskindrow@gmail.com>
2024-06-24 15:29:20 -0300

    adding tags and definitions to swagger.js, adding swagger tags on toolsControllers

--- api/controllers/toolsController.js
+++ api/controllers/toolsController.js
@@ -12,6 +12,15 @@ module.exports = () => {
     const controller = {};
   
     controller.listTools = (req, res) => {
+      // #swagger.tags = ['Tools']
+      // #swagger.description = 'Endpoint para obter as ferramentas.'
+
+      /* #swagger.parameters['tag'] = {
+	        in: 'query',
+          description: 'Um tag qualquer.',
+          type: 'string'
+        } */
+
       if (req.query.tag) {
         pool.query('SELECT * FROM tools WHERE $1 = ANY(tags);', [req.query.tag], (error, results) => {
           if (error) {
@@ -25,28 +34,47 @@ module.exports = () => {
             throw error;
           }
           res.status(200).json(results.rows);
+          /* #swagger.responses[200] = { 
+              schema: { $ref: "#/definitions/Tools" },
+              description: 'Ferramenta(s) encontradas.' 
+          } */
         });
       }
     }
 
     controller.saveTool = (req, res) => {
+      // #swagger.tags = ['Tools']
+      // #swagger.description = 'Endpoint para adicionar uma ferramenta.'
+
+      /* #swagger.parameters['newTool'] = {
+               in: 'body',
+               description: 'Informações da ferramenta',
+               required: true,
+               schema: { $ref: "#/definitions/AddTool" }
+        } */
+
       pool.query('INSERT INTO tools (title, link, description, tags) VALUES ($1, $2, $3, $4) RETURNING *;', 
       [req.body.title, req.body.link, req.body.description, req.body.tags],
       (error, results) => {
         if (error) {
           throw error;
         }
+        // #swagger.responses[201] = { description: 'Ferramenta registrada com sucesso!' }
         res.status(201).json(results.rows[0]);
       });
     }
 
     controller.deleteTool = (req, res) => {
+      // #swagger.tags = ['Tools']
+      // #swagger.description = 'Endpoint para remover uma ferramenta'
+      // #swagger.parameters['id'] = { description: 'ID da ferramenta' }
       pool.query('DELETE FROM tools WHERE id = $1;', 
       [req.params.id],
       (error, results) => {
         if (error) {
           throw error;
         }
+         /* #swagger.responses[200] = {description: 'Ferramenta removida.'} */
         res.status(200).json(results.rows);
       });
     }
--- swagger.js
+++ swagger.js
@@ -14,6 +14,33 @@ const doc = {
     schemes: ['http', 'https'],
     consumes: ['application/json'],
     produces: ['application/json'],
+    tags: [
+        {
+            "name": "Tools",
+            "description": "Endpoints"
+        }
+    ],
+    definitions: {
+        Tools: {
+            id: 0,
+            $title: "Notion",
+            $link: "https://notion.so",
+            $description: "All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.",
+            $tags: [
+                "organization",
+                "planning",
+                "collaboration",
+                "writing",
+                "calendar"
+            ]
+        },
+        AddTool: {
+            $title: "hotel",
+            $link: "https://github.com/typicode/hotel",
+            $description: "Local app manager. Start apps within your browser, developer tool with local .localhost domain and https out of the box.",
+            $tags: ["node", "organizing", "webapps", "domain", "developer", "https", "proxy"]
+        }
+    }
 }
 
 
